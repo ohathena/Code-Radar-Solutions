dd17b02bb24a563b8dacad9b8f9b4f32cf38efc1
Trisha <isthat.fubuki@gmail.com>
2025-01-21 10:27:40 +0530

    Solution to Find Day of the Week

--- Find Day of the Week@@6788dd63490637cfc44e1ba7/code.c
+++ Find Day of the Week@@6788dd63490637cfc44e1ba7/code.c
@@ -3,7 +3,8 @@
 int main() {
     int a;
     scanf("%d" , &a);
-    const char* days[] = {"Monday" , "Tuesday" , "Wednesday" , "Thursday" , "Friday" , "Saturday" , "Sunday"};
-    printf("%s", days[a-1]);
+    printf("%d" , a);
+    // const char* days[] = {"Monday" , "Tuesday" , "Wednesday" , "Thursday" , "Friday" , "Saturday" , "Sunday"};
+    // printf("%s", days[a-1]);
     return 0;
 }
