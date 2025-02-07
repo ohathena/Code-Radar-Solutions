6336ce33f5e605235ae2f719d8ac0647bb699c4a
Trisha <isthat.fubuki@gmail.com>
2025-02-07 11:53:25 +0530

    Solution to Find the Frequency of Elements in an Array

--- Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
+++ Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
@@ -6,7 +6,23 @@ int main() {
     for(int i=0; i<n ; i++){
         scanf("%d" , &arr[i]);
     }
- 
+    int copy=[];
+    for(int i=0; i<n ; i++){
+        copy[i] = arr[i];
+    }
+t
+    for (i = 0; i < n; i++) {
+        for (j = i + 1; j < n; j++) {
+            if (copy[i] == copy[j]) {
+                for (k = j; k < n - 1; k++) {
+                    copy[k] = copy[k + 1];
+                }
+                n--; 
+                j--;
+            }
+        }
+    }
+
     int temp;
     for(int i=0; i<n-1; i++){
         for(int j=0; j<n-i-1 ; j++){
@@ -17,31 +33,42 @@ int main() {
             }
         }  
    }
+   int r = sizeof(copy);
+  for(int i=0; i<r; i++){
+    int y=1;
+         for(int j=0; j<n ; j++){
+            if(arr[j] == copy[i]){
+                y++;
+            }
+     }
+     printf("%d %d \n", copy[i] , y);
+     }
+
     // for(int i=0; i<n ; i++){
     //     printf("%d " , arr[i]);
     // }
    
-   int y=1;
-    for(int i=0; i<=n-1 ; i++){
-     if(i<n-1){
-        if(arr[i] != arr[i+1]){
-            printf("%d %d \n", arr[i] , y);
-            y =1;
-        }
-        else{
-            y++;
-        }
-     } 
-     else{
-        if(arr[i] == arr[i-1]){
-            // y+=1;
-            printf("%d %d \n", arr[i] , y);
-        }
-        else{
-            printf("%d %d \n", arr[i] , y);
-        }
-     }
-     }
+//    int y=1;
+//     for(int i=0; i<=n-1 ; i++){
+//      if(i<n-1){
+//         if(arr[i] != arr[i+1]){
+//             printf("%d %d \n", arr[i] , y);
+//             y =1;
+//         }
+//         else{
+//             y++;
+//         }
+//      } 
+//      else{
+//         if(arr[i] == arr[i-1]){
+//             // y+=1;
+//             printf("%d %d \n", arr[i] , y);
+//         }
+//         else{
+//             printf("%d %d \n", arr[i] , y);
+//         }
+//      }
+//      }
 
    
     return 0;
