5ad5f718a1556a1907f322d6002ee36468522e44
Trisha <isthat.fubuki@gmail.com>
2025-02-07 17:59:12 +0530

    Solution to Count the Number of Prime Numbers in an Array

--- Count the Number of Prime Numbers in an Array@@676c02002dbc8c3e7a280b7d/code.c
+++ Count the Number of Prime Numbers in an Array@@676c02002dbc8c3e7a280b7d/code.c
@@ -4,43 +4,48 @@ int main() {
     int c=0;
     scanf("%d" , &n);
     int ar[n];
+    int temp[n];
     for(int i=0; i<n ; i++){
         scanf("%d" , &ar[i]);
     }
      for(int i=0; i<n ; i++){
-    //     if(ar[i] == 2 || ar[i] == 3){
-    //         c++;
-    //     }
-    //     else if(ar[i]==0 || ar[i]==1){
-    //         continue;
-    //     }
-    //     else{
-    //         int t=0;
-    //         for(int j=2; j<i ; j++){
-    //             if(ar[i]%j==0){
-    //                 t =1;
-    //                 break;
-    //             }  
-    //         }
-          
-    //         if(t==0){
-    //              c++;
-    //       }
-    //     }
-    //  }
-
-     int d=0;
-        for(int i=2 ; i<ar[i] ; i++){
-            if(ar[i]%i==0){
-                d++;
+         if(ar[i] == 2 || ar[i] == 3){
+             temp[c]==ar[i];
+             c++;
+         }
+         else if(ar[i]==0 || ar[i]==1){
+             continue;
+         }
+         else{
+             int t=0;
+             for(int j=2; j<i ; j++){
+                 if(ar[i]%j==0){
+                    t++;
+                    break;
+                }  
             }
+          
+            if(t==0){
+               temp[c]== ar[i];
+                 c++;
+          }
         }
-        if(d==0){
-              c++;   
-        }
+     }
+     int l = sizeof(ar)/ sizeof(ar[0]);
+     
+
+    //  int d=0;
+    //     for(int i=2 ; i<ar[i] ; i++){
+    //         if(ar[i]%i==0){
+    //             d++;
+    //         }
+    //     }
+    //     if(d==0){
+    //           c++;   
+    //     }
        
-    }
-     printf("%d" , c);
+    // }
+    //  printf("%d" , c);
     
-    return 0;
+    return l;
 }
