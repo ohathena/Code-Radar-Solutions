be0e69926ef5abdd72a214d0885abbf432b2df37
Trisha <isthat.fubuki@gmail.com>
2025-02-08 10:33:56 +0530

    Solution to Find and Display the Top Scorer

--- Find and Display the Top Scorer@@67877d61dc107486ad5c2744/code.c
+++ Find and Display the Top Scorer@@67877d61dc107486ad5c2744/code.c
@@ -15,37 +15,25 @@ int main() {
     int i;
 
     for (i = 0; i < n; i++) {
-     
-        
-    
         scanf("%d", &students[i].roll);
-        
-    
         scanf("%f", &students[i].marks);
         
-        getchar(); // Consume newline left in buffer
-
-  
+        while (getchar() != '\n');
         fgets(students[i].name, sizeof(students[i].name), stdin);
-
-        // Remove trailing newline from fgets()
         size_t len = strlen(students[i].name);
         if (len > 0 && students[i].name[len - 1] == '\n') {
             students[i].name[len - 1] = '\0';
         }
     }
 
-    // Assume first student has highest marks
     topStudent = students[0];
 
-    // Find the student with highest marks
     for (i = 1; i < n; i++) {
         if (students[i].marks > topStudent.marks) {
             topStudent = students[i];
         }
     }
 
-    // Display top scorer
     printf("\nTop Scorer:\n");
     printf("Roll Number: %d, ", topStudent.roll);
     printf("Name: %s, ", topStudent.name);
