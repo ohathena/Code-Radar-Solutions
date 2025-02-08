8e869687ca76057ecec56b5a500f73c1838dcd8e
Trisha <isthat.fubuki@gmail.com>
2025-02-08 11:30:21 +0530

    Solution to Categorize Students Based on Grades

--- Categorize Students Based on Grades@@67877d61dc107486ad5c274e/code.c
+++ Categorize Students Based on Grades@@67877d61dc107486ad5c274e/code.c
@@ -8,32 +8,32 @@ struct Student {
 };
 
 int main() {
-    int n;
-    scanf("%d", &n);
+    // int n;
+    // scanf("%d", &n);
 
-    struct Student students[n], theStudent;
-    int i;
+    // struct Student students[n];
+    // int i;
 
 
-    for (i = 0; i < n; i++) {
-        scanf("%d", &students[i].roll);
-        scanf("%s", students[i].name);
-        scanf("%f", &students[i].marks);
-    }
+    // for (i = 0; i < n; i++) {
+    //     scanf("%d", &students[i].roll);
+    //     scanf("%s", students[i].name);
+    //     scanf("%f", &students[i].marks);
+    // }
 
-    int c = 0;
 
-    for (i = 0; i <= n; i++) {
-        if (students[i].marks >= 85) {
-              printf("Roll Number: %d, Name: %s, Grade: A" ,students[i].roll, students[i].name );
-        }
-        else if(students[i].marks >=70){
-            printf("Roll Number: %d, Name: %s, Grade: B" ,students[i].roll, students[i].name );
-        }
-        else{
-         printf("Roll Number: %d, Name: %s, Grade: C" ,students[i].roll, students[i].name ); 
-        }
-    }
+
+    // for (i = 0; i <= n; i++) {
+    //     if (students[i].marks >= 85) {
+    //           printf("Roll Number: %d, Name: %s, Grade: A" ,students[i].roll, students[i].name );
+    //     }
+    //     else if(students[i].marks >=70){
+    //         printf("Roll Number: %d, Name: %s, Grade: B" ,students[i].roll, students[i].name );
+    //     }
+    //     else{
+    //      printf("Roll Number: %d, Name: %s, Grade: C" ,students[i].roll, students[i].name ); 
+    //     }
+    // }
 
         
 
