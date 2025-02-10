7f9e18ef5ab2d1ca449b6b35cce9b6364a5db773
Trisha <isthat.fubuki@gmail.com>
2025-02-10 17:24:24 +0530

    Solution to Toll Plaza Management System

--- Toll Plaza Management System@@67877d61dc107486ad5c2750/code.c
+++ Toll Plaza Management System@@67877d61dc107486ad5c2750/code.c
@@ -1,10 +1,40 @@
 #include <stdio.h>
+#include <string.h>
 
-char* welcome() {
-    return "Welcome to Code Radar!";
-}
+struct Student {
+    char name[50];
+    int roll;
+    float salary;
+};
 
 int main() {
-    printf("%s", welcome());
+    int n;
+    scanf("%d", &n);
+
+    struct Student students[n], theStudent;
+    int i;
+    float x = 0.00;
+    float y = 0.00;
+    float x = 0.00;
+
+    for (i = 1; i <= n; i++) {
+        scanf("%s", &students[i].roll);
+        scanf("%s", &students[i].name);
+        scanf("%f", &students[i].salary);
+        if (students[i].name == "Car") {
+            x+= students[i].salary;
+        }
+        else if(students[i].name=="Truck"){
+            y+= students[i].salary;
+        }
+        else{
+           z+= students[i].salary;
+        }
+     
+
+    }
+       printf("Cars: %.2f, Trucks: %.2f, Bikes: %.2f\n", x ,y,z );
+     
+
     return 0;
 }
