486d411f57ae4dff18afb628b91bcab8ded62263
TRISHA KHAJURIA <isthat.fubuki@gmail.com>
2025-03-10 12:26:47 +0530

    Solution to Count the Occurrences of a Character

--- Count the Occurrences of a Character@@676c039d80e1714600f6764b/code.c
+++ Count the Occurrences of a Character@@676c039d80e1714600f6764b/code.c
@@ -4,19 +4,10 @@
 int main() {
     char str1[100];
     char d;
-
-    // Input string using fgets (reads the entire line)
     fgets(str1, sizeof(str1), stdin);
-    
-    // Remove the newline character if present
-    str1[strcspn(str1, "\n")] = '\0';
-
-    // Input the character
+  //  str1[strcspn(str1, "\n")] = '\0';
     scanf("%c", &d);
-
     int c = 0;
-    
-    // Count occurrences of 'd' in the string
     for (int i = 0; str1[i] != '\0'; i++) {
         if (str1[i] == d) {
             c++;
