38d46a21f9b6327161c2140b565143e34b8a0e73
TRISHA KHAJURIA <isthat.fubuki@gmail.com>
2025-03-19 18:40:15 +0530

    Solution to Smallest Window Sort

--- Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
+++ Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
@@ -1,30 +1,37 @@
-#include <stdlib.h>  
-int compare(const void *a, const void *b) {
-    return (*(int*)a - *(int*)b);
-}
-
-int findUnsortedSubarray(int arr[], int n) {
-    int sortedArr[n]; 
-
-    for (int i = 0; i < n; i++) {
-        sortedArr[i] = arr[i];
+// Your code here...
+int findUnsortedSubarray(int arr[],int n){
+    int curr, prev, arr2[100];
+     for(int i=0;i<n;i++){
+        arr2[i]=arr[i];
     }
-
-   
-    qsort(sortedArr, n, sizeof(int), compare);
-
- 
-    int left = 0;
-    while (left < n && arr[left] == sortedArr[left]) {
-        left++;
+    for(int i=1;i<n;i++){
+        curr=arr2[i];
+        prev=i-1;
+        while(prev >=0 && arr2[prev] > curr){
+            arr2[prev + 1] = arr2[prev];
+            --prev;
+        }
+        arr2[prev+1]=curr;
     }
-
-    if (left == n) return 0;
-
-    int right = n - 1;
-    while (right > left && arr[right] == sortedArr[right]) {
-        right--;
+    
+    int start=-1;
+    int end=-1;
+    for(int i=0;i<n;i++){
+        if(arr[i]!=arr2[i]){
+                start=i;
+            break;
+        }
     }
+    for(int i=n-1;i>=0;i--){
+        if(arr[i]!=arr2[i]){
+            end=i;
+            break;
+        }
+    }
+    if(start==-1 && end==-1){
+        return 0;
+    }
+    int ans=end-start+1;
+    return ans;
 
-    return right - left + 1;
 }
